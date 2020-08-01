5e71985aebb40b45bb2cc839341ed2f99f6487df
Paraprax <parkerjohnston28@gmail.com>
2020-08-01 18:21:22 -0400

    arrary of blanks now auto-generates to match current secret word length

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,7 @@
 /*
 
+1.5 Generate a line of underscores equivalent to the number of letters in the password
+
 2. Have the user guess letters
 
 3. Have the guessed letters checked against the letters in the secret word
@@ -13,17 +15,19 @@
 */
 
 // initial vars:
-const passwordArray = ['modem', 'matrix', 'wireframe', 'bandwidth', 'giraffe', 'octopus', 'martini', 'hexagon', 'xylophone', 'delta', 'ultramarine', 'zeitgeist', 'rook', 'xenon', 'xerox', 'megabyte', 'enterprise', 'nautilus', 'oppenheimer', 'caffeine', 'adrenaline', 'firewall', 'zeppelin', 'phreak', 'touchtone', 'jupiter', 'quarrel', 'vespertine']
+const passwordArray = ['modem', 'matrix', 'wireframe', 'bandwidth', 'giraffe', 'octopus', 'martini', 'hexagon', 'xylophone', 'delta', 'ultramarine', 'zeitgeist', 'rook', 'krypton', 'xerox', 'megabyte', 'enterprise', 'nautilus', 'oppenheimer', 'caffeine', 'adrenaline', 'firewall', 'zeppelin', 'phreak', 'touchtone', 'jupiter', 'quartet', 'vespertine', 'zeroes', 'flux', 'dialup', 'quaver'];
 let password = "";
+let blanks = [];
 let wordsFound = 0;
 
 const newRound = () => {
     passwordPicker();
+    passwordBlanks(password);
 }
 
 // function to choose one password from the array per round:
 const passwordPicker = () => {
-    password = passwordArray[Math.floor(Math.random() * 28)];
+    password = passwordArray[Math.floor(Math.random() * 32)];
 }
 
 // function to keep updating the html to match the game data:
@@ -31,7 +35,14 @@ const screenUpdater = () => {
     document.getElementById('overridden').innerHTML = wordsFound;
 }
 
+//
+const passwordBlanks = (word) => {
+    for (var i = 0; i < word.length; i++) {
+        blanks.push(" _ ");
+    }
+    document.getElementById('password-blanks').innerHTML = blanks;
+}
+
 //newRound is first called on page load:
 newRound();
-
 
