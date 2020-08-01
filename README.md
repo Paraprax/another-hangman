ac67fc1c004b0201d58f75210035f899b31c16c6
Paraprax <parkerjohnston28@gmail.com>
2020-08-01 03:53:26 -0400

    added preliminary divs and pseudocode comments outlining initial functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # another-hangman
+
+a 'hangman'-esque game with an oldschool hacker theme
---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+/*
+1. Generate a secret word
+
+2. Have the user guess letters
+
+3. Have the guessed letters checked against the letters in the secret word
+    - if they're in there, fill them in the blank boxes
+    - if they're not, add them to the wrong letters array
+
+4. Trigger a win event if the user guesses all the letters
+
+5. Trigger a loss event if the user guesses eight wrong letters
+
+*/
