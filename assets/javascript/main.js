5fa0ec1dc7aeaa925c596ca7cbfd39885e5c5913
Paraprax <parkerjohnston28@gmail.com>
2020-08-03 01:34:39 -0400

    starting to config bootstrap containers

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,5 @@
 /*
 
-3. Have the guessed letters checked against the letters in the secret word
-    - if they're in there, fill them in the blank boxes P
-    - if they're not, add them to the wrong letters array
-
-4. Trigger a win event if the user guesses all the letters
-
 5. Trigger a loss event if the user guesses eight wrong letters
 
 */
